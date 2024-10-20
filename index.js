78953829215419750d6edfa025ebfdcd41826b87
Use4ul <i.am.use4ul@gmail.com>
2024-10-20 22:35:25 +0300

    port3001

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const swaggerUi = require('swagger-ui-express');
 
 const swaggerDocument = require('./swagger/swaggerDocs/swagger.main.json');
